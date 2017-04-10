341376af4de16490e14c9bb5201def2eb15b67bb
revanorion <revanorion@aol.com>
2017-04-10 10:09:42 -0400

    Database Changes

--- DB_SQL.sql
+++ DB_SQL.sql
@@ -1,18 +1,12 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     4/7/2017 11:54:17 AM                         */
+/* Created on:     4/10/2017 10:07:16 AM                        */
 /*==============================================================*/
 
 
 drop table if exists FILE;
 
 drop table if exists FILE_HISTORY;
-
-drop index INDEX_1 on USERS;
-
-drop table if exists USERS;
-
-drop table if exists USER_PERMISSIONS;
 
 drop table if exists WALL;
 
@@ -50,44 +44,6 @@ create table FILE_HISTORY
 );
 
 /*==============================================================*/
-/* Table: USERS                                                 */
-/*==============================================================*/
-create table USERS
-(
-   USER_SEQ             int not null auto_increment,
-   USER_PERMISSIONS_SEQ int,
-   USERNAME             varchar(255) not null,
-   PASSWORD             varchar(255) not null,
-   CREATED_ON           datetime,
-   CREATED_BY           varchar(200),
-   CHANGED_ON           datetime,
-   CHANGED_BY           varchar(200),
-   primary key (USER_SEQ)
-);
-
-/*==============================================================*/
-/* Index: INDEX_1                                               */
-/*==============================================================*/
-create unique index INDEX_1 on USERS
-(
-   USERNAME
-);
-
-/*==============================================================*/
-/* Table: USER_PERMISSIONS                                      */
-/*==============================================================*/
-create table USER_PERMISSIONS
-(
-   USER_PERMISSIONS_SEQ int not null auto_increment,
-   PERMISSIONS_CODE     varchar(20) not null,
-   CREATED_ON           datetime,
-   CREATED_BY           varchar(200),
-   CHANGED_ON           datetime,
-   CHANGED_BY           varchar(200),
-   primary key (USER_PERMISSIONS_SEQ)
-);
-
-/*==============================================================*/
 /* Table: WALL                                                  */
 /*==============================================================*/
 create table WALL
@@ -100,7 +56,6 @@ create table WALL
    CHANGED_ON           datetime,
    CHANGED_BY           varchar(200),
    USER_SID             varchar(200),
-   USER_SEQ             int,
    primary key (WALL_SEQ)
 );
 
@@ -118,7 +73,6 @@ create table WALL_COMMENT
    CHANGED_ON           datetime,
    CHANGED_BY           varchar(200),
    USER_SID             varchar(200),
-   USER_SEQ             int,
    primary key (WALL_COMMENT_SEQ)
 );
 
@@ -145,12 +99,8 @@ create table WALL_LIKE
    CREATED_BY           varchar(200),
    CHANGED_ON           datetime,
    CHANGED_BY           varchar(200),
-   USER_SID             varchar(200),
-   USER_SEQ             int
+   USER_SID             varchar(200)
 );
-
-alter table USERS add constraint FK_REFERENCE_8 foreign key (USER_PERMISSIONS_SEQ)
-      references USER_PERMISSIONS (USER_PERMISSIONS_SEQ) on delete restrict on update restrict;
 
 alter table WALL_COMMENT add constraint FK_REFERENCE_6 foreign key (WALL_SEQ)
       references WALL (WALL_SEQ) on delete restrict on update restrict;
